2b7d1213723dac95a668a318a8500c9321ec2a25
sunithaposa <48434881+sunithaposa@users.noreply.github.com>
2019-11-11 13:01:30 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -23,23 +23,25 @@ def webhook():
 	return r
 
 # extract parameter values, query weahter api, construct the resposne
- def makeResponse(req):
-		result=req.get("result")
-		parameters=result.get("paramters")
-		city=parameters.get("geo-city")
-		date=parameters.get("date")
-		r=request.get('http://pro.openweathermap.org/data/2.5/forecast/hourly?q='+city+',us&appid=b6907d289e10d714a6e88b30761fae22')
-		json_object=r.json()
-		weather=json_object('list')
-		for i in len(weather):
-			if date in weather[i]['dt_txt']:
-				condition=weather[i]['weather'][0]['description']
-		speech="The forecast for "+city+ "for "_date+" is"+condition
-		return {
-			"speech": speech,
-			"displayText":speech,
-			"source":"apiai-weather-webhook"
-		}
+def makeResponse(req):
+	result=req.get("result")
+	parameters=result.get("paramters")
+	city=parameters.get("geo-city")
+	date=parameters.get("date")r
+	=request.get('http://pro.openweathermap.org/data/2.5/forecast/hourly?q='+city+',us&appid=b6907d289e10d714a6e88b30761fae22')
+	json_object=r.json()
+	weather=json_object('list')
+	
+
+	for i in len(weather):
+		if date in weather[i]['dt_txt']:
+			condition=weather[i]['weather'][0]['description']
+	speech="The forecast for "+city+ "for "_date+" is"+condition
+	return {
+		"speech": speech,
+		"displayText":speech,
+		"source":"apiai-weather-webhook"}
+	
 if __name__=__main__:
 	port=int(os.getenv('PORT',5000))
 	print("starting on port %d" % port)
