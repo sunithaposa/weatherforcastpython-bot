29637b65241f9d67e638199ac28f177bccd37764
sunithaposa <48434881+sunithaposa@users.noreply.github.com>
2019-11-11 15:02:31 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -35,7 +35,7 @@ def makeResponse(req):
 	weather=json_object['list']
 	
 
-	for i in range(0,30):
+	for i in range(0,100):
 		if date in weather[i]['dt_txt']:
 			condition=weather[i]['weather'][0]['description']
 	speech="The forecast for "+city+ "for "+date+" is"+condition
